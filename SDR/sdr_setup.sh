902a3b23e2be16ca8062ea1b0c544b10c5857fda
DSUmjham <>
2018-05-29 10:53:10 -0500

    Updated symlinks in gqrx setup

--- SDR/sdr_setup.sh
+++ SDR/sdr_setup.sh
@@ -3,6 +3,7 @@
 # Download all of the software and dependencies
 apt-get update
 apt-get install gqrx pkg-config librtlsdr-dev -y
+ln -sf /usr/lib/x86_64-linux-gnu/libvolk.so.1.3.1 /usr/lib/x86_64-linux-gnu/libvolk.so.1.3
 
 # Get dump1090 for decoding ADS-B and isntall
 git clone https://github.com/antirez/dump1090.git /opt/dump1090
