48b6a1efa36f8c59fe74ed8bd1e56dfb03048115
Michael Ham <mjham@mjhdsu14.dsuia.lan>
2019-06-09 19:20:24 -0500

    Added Audacity to sdr_setup.sh, updated README.MD

--- SDR/sdr_setup.sh
+++ SDR/sdr_setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # Download all of the software and dependencies
-apt-get update
-apt-get install gqrx pkg-config librtlsdr-dev -y
+apt update
+apt install gqrx pkg-config librtlsdr-dev -y
 ln -sf /usr/lib/x86_64-linux-gnu/libvolk.so.1.3.1 /usr/lib/x86_64-linux-gnu/libvolk.so.1.3
 
 # Get dump1090 for decoding ADS-B and isntall
 git clone https://github.com/antirez/dump1090.git /opt/dump1090
 cd /opt/dump1090
 make 
+
+# Downlaod and install audacity
+apt install audacity -y
