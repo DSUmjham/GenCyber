56c79a9746e0c53677688e9ceae165ec2bc4d51f
DSUmjham <DSUmjham@github.com>
2021-06-07 14:42:35 -0500

    Updated setup script

--- SDR/sdr_setup.sh
+++ SDR/sdr_setup.sh
@@ -5,6 +5,9 @@ apt update
 apt install gqrx pkg-config librtlsdr-dev -y
 ln -sf /usr/lib/x86_64-linux-gnu/libvolk.so.1.3.1 /usr/lib/x86_64-linux-gnu/libvolk.so.1.3
 
+# Blacklist the official SDR drivers
+echo 'blacklist dvb_usb_rtl28xxu' | sudo tee --append /etc/modprobe.d/blacklist-dvb_usb_rtl28xxu.conf
+
 # Get dump1090 for decoding ADS-B and isntall
 git clone https://github.com/antirez/dump1090.git /opt/dump1090
 cd /opt/dump1090
