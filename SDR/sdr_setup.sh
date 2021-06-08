0b3d01e2c2f30cad833b9609006452d13eff317f
DSUmjham <DSUmjham@github.com>
2021-06-08 16:15:43 -0500

    updated script for dump1090-fa

--- SDR/sdr_setup.sh
+++ SDR/sdr_setup.sh
@@ -1,24 +1,31 @@
-#!/bin/bash
+#!/usr/bin/zsh
 
-# Download all of the software and dependencies
+# check perms
+if [[ $EUID -ne 0 ]]; then
+        echo "This script must be run as root or with sudo"
+        exit 1
+fi
+
+# download all of the software and dependencies
 apt update
 apt install gqrx pkg-config librtlsdr-dev -y
 ln -sf /usr/lib/x86_64-linux-gnu/libvolk.so.1.3.1 /usr/lib/x86_64-linux-gnu/libvolk.so.1.3
 
-# Blacklist the official SDR drivers
-echo 'blacklist dvb_usb_rtl28xxu' | sudo tee --append /etc/modprobe.d/blacklist-dvb_usb_rtl28xxu.conf
-
-# Get dump1090 for decoding ADS-B and isntall
-git clone https://github.com/antirez/dump1090.git /opt/dump1090
-cd /opt/dump1090
-make 
-
-# Downlaod and install audacity
+# downlaod and install audacity
 apt install audacity -y
 
-# Install rfcat for transmitting 
+# install rfcat for transmitting 
 apt  install rfcat -y
 
-# Stop the kernel from taking over the SDR
-echo "blacklist dvb_usb_rtl28xxu" >> /etc/modprobe.d/dvb-blacklist.conf
-reboot
+# clean up any old ADSB installs
+rm -rf /opt/dump1090
+rm -rf /usr/local/lib/librtlsdr*
+
+# build the new version of dump1090-fa
+cd /opt
+sudo bash -c "$(wget -O - https://raw.githubusercontent.com/abcd567a/piaware-ubuntu20-amd64/master/install-dump1090-fa.sh)"
+
+# blacklist the default TV tuner driver
+echo 'blacklist dvb_usb_rtl28xxu' | sudo tee --append /etc/modprobe.d/blacklist-dvb_usb_rtl28xxu.conf
+
+echo 'Script finished, look for errors and reboot'
