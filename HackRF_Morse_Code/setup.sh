93c47cb94a0871796dc7ec0dd27ffa3d4cc53c0d
Michael Ham <mike.ham@dsu.edu>
2025-06-11 11:44:41 -0500

    updates setup for Kali

--- HackRF_Morse_Code/setup.sh
+++ HackRF_Morse_Code/setup.sh
@@ -5,7 +5,12 @@ echo "Updating package list..."
 sudo apt update
 
 echo "Installing system packages: Python3, pip, HackRF, GQRX, Audacity..."
-sudo apt install -y python3 python3-pip hackrf gqrx-sdr audacity
+
+sudo apt install -y python3 python3-pip hackrf gqrx-sdr audacity aplay
 
 echo "Installing required Python packages (numpy, scipy)..."
-pip3 install --user numpy scipy
+python3 -m venv ~/py_envs
+source ~/py_envs/bin/activate
+python3 -m pip install numpy
+
+pip3 install numpy scipy
