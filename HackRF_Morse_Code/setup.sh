26daa01f8803fdd8459cb726a1b79e1fe317f1ed
Michael Ham <mike.ham@dsu.edu>
2025-06-11 17:01:18 -0500

    adds alsa-utils

--- HackRF_Morse_Code/setup.sh
+++ HackRF_Morse_Code/setup.sh
@@ -6,7 +6,7 @@ sudo apt update
 
 echo "Installing system packages: Python3, pip, HackRF, GQRX, Audacity..."
 
-sudo apt install -y python3 python3-pip hackrf gqrx-sdr audacity aplay
+sudo apt install -y python3 python3-pip hackrf gqrx-sdr audacity alsa-utils
 
 echo "Installing required Python packages (numpy, scipy)..."
 python3 -m venv ~/py_envs
